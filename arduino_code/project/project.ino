5dc0e72a32a67ba79f7fe59e7f501bbe11d2874c
LevapsP <2pavlik.kan@gmail.com>
2025-10-02 14:09:44 +0300

    Added sending data to server

--- arduino_code/project/project.ino
+++ arduino_code/project/project.ino
@@ -1,8 +1,22 @@
-//base code for testing mpu6050 with esp32
+//sending data to server
+
+
 #include <Wire.h>
 #include <MPU6050.h>
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 MPU6050 mpu;
+
+const int SAMPLE_RATE = 10;
+const int WINDOW_SIZE = 75;
+const int NUM_AXES = 6;
+
+float dataBuffer[WINDOW_SIZE][NUM_AXES];
+int sampleCount;
+
+const size_t JSON_DOC_SIZE = 16384;
 
 void setup() {
   Serial.begin(115200);
@@ -16,21 +30,102 @@ void setup() {
   mpu.calibrateGyro();
   mpu.setThreshold();
 
+  const char* ssid = "ssid";
+  const char* password = "password";
+
+  wifi.begin(ssid, password));
+
+  Serial.println("Connecting to wifi");
+  while(wifi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(200);
+  }
+
+  Serial.println("Connected to wifi");
+  Serial.print("IP:");
+  Serial.println(wifi.localIP());
+
 }
 
 void loop() {
-  Vector rawAccel = mpu.readRawAccel();
-  Vector rawGyro = mpu.readRawGyro();
+  static unsigned long lastSampleTime;
 
-  Serial.print("ax: "); Serial.print(rawAccel.XAxis);
-  Serial.print(" | ay: "); Serial.print(rawAccel.YAxis);
-  Serial.print(" | az: "); Serial.print(rawAccel.ZAxis);
+  if(millis() - lastSampleTime >= SAMPLE_RATE)
+  {
+    sensor_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
 
-  Serial.print(" || gx: "); Serial.print(rawGyro.XAxis);
-  Serial.print(" | gy: "); Serial.print(rawGyro.YAxis);
-  Serial.print(" | gz: "); Serial.println(rawGyro.ZAxis);
+    if(sampleCount < WINDOW_SIZE)
+    {
+      dataBuffer[sampleCount][0] = a.acceleration.x();
+      dataBuffer[sampleCount][1] = a.acceleration.y();
+      dataBuffer[sampleCount][2] = a.acceleration.z();
+      dataBuffer[sampleCount][3] = g.gyro().x();
+      dataBuffer[sampleCount][4] = g.gyro().y();
+      dataBuffer[sampleCount][5] = g.gyro().z();
 
-  Serial.println("----------------------------")
-  delay(200);
+      sampleCount++;
+
+    }
+
+    lastSampleTomi = millis();
+
+    if(sampleCount >= WINDOW_SIZE)
+    {
+      if(wifi.status() == WL_CONNECTED)
+      {
+        sendDataToServer();
+      }
+
+      sampleCount = 0;
+    }
+  }
+
+}
+
+void sendDataToServer()
+{
+  StaticJsonDocument<JSON_DOC_SIZE> doc;
+
+  doc["devic_id"] = "ESP32_GLOVE_02";
+
+  JsonArra dataArray = doc.createNestedArray("data");
+
+  for(int i = 0; i < WINDOW_SIZE; i++)
+  {
+    JsonArray sample = dataArraiy.createNestedArray();
+
+    for(int j = 0; j < NUM_AXES; j++)
+    {
+      sample.add(dataBuffer[i][j]);
+    }
+  }
+
+  String payloadJson;
+  serializeJson(doc, payloadJson);
+
+  HTTPClient http;
+
+  http.begin("server");
+  http.addHeader("Content-Type", "application/json");
+
+  int httpResponceCode = http.POST(payloadJson);
+
+  if(http.responcecode > 0)
+  {
+    String responce = http.getString();
+    Serial.print("HTTP code: ");
+    Serial.println(httpResponceCode);
+    Serial.print("Server response: ");
+    Serial.println(responce);
+    
+  }
+  else
+  {
+    Serial.print("HTTP error: ");
+    Serial.println(httpResponceCode);
+  }
+  http.end();
 
 }
