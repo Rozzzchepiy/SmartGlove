c8dd10ef6712d7ccf31647d5ad1aa2ff11d97bbb
Dmytro <150160672+DmytroKyryliuk2023@users.noreply.github.com>
2025-11-29 16:23:32 +0200

    Clarify Python version requirement in comment
    Updated comment to specify Python version requirement.

--- form_requirements.ps1
+++ form_requirements.ps1
@@ -1,2 +1,2 @@
-# Генеруємо requirements.txt
+# Генеруємо requirements.txt (для встановлення всіх пакетів, які генеруються, потрібний python 3.11)
 pipreqs . --encoding=utf-8 --ignore .venv,__pycache__,build,dist
